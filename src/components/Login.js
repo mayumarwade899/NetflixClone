bb8723ca70530bd9972d4304eefae440fd69edbc
Mayur Marwade <mayumarwade899@gmail.com>
2024-04-30 08:59:35 +0530

    Logo & BG image added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Header />
+      <div>
+        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/058eee37-6c24-403a-95bd-7d85d3260ae1/e10ba8a6-b96a-4308-bee4-76fab1ebd6ca/IN-en-20240422-POP_SIGNUP_TWO_WEEKS-perspective_WEB_db9348f2-4d68-4934-b495-6d9d1be5917e_large.jpg' alt='bg-image' />
+      </div>
+    </div>
   )
 }
 
